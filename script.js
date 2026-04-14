049ab652d5cf5911e14b9baf70b4a1f7240ac26e
vipransh4 <vipransh4@gmail.com>
2026-04-15 00:58:21 +0530

    changing script to connect

--- script.js
+++ script.js
@@ -12,15 +12,8 @@ import handleClarifai from "./controllers/clarifai.js";
 
 const db = knex({ 
   client: 'pg', 
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl:{rejectUnauthorized:false},
-    host:process.env.DATABASE_HOST,
-    port: 5432, 
-    user: process.env.DATABASE_USER, 
-    password: process.env.DATABASE_PW, 
-    database: process.env.DATABASE_DB 
-  },
+  connection: process.env.DATABASE_URL,
+  ssl:{rejectUnauthorized:false}
 });
 
 const app=express();
