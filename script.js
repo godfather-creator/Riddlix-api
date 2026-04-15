2e5d50df0916dbfada91eaa2abb7a9f85d16dad8
vipransh4 <vipransh4@gmail.com>
2026-04-16 00:24:48 +0530

    fixing

--- script.js
+++ script.js
@@ -10,10 +10,12 @@ import handleProfileGet from "./controllers/profile.js";
 import handleImage from "./controllers/image.js";
 import handleClarifai from "./controllers/clarifai.js";
 
-const db = knex({ 
-  client: 'pg', 
-  connection: process.env.DATABASE_URL,
-  ssl:{rejectUnauthorized:false}
+const db = knex({
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  }
 });
 
 const app=express();
